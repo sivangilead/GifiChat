87a2f50a2335ab88fd56ab997647976d43328dc7
Sivan Gilead <sivan.gilad@gmail.com>
2018-08-01 22:51:58 -0400

    add electron

---
+++ release-builds/gifichat-darwin-x64/gifichat.app/Contents/Resources/app/client/routes.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { withRouter, Route, Switch } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { Login, Signup, UserHome } from './components'
+import { me } from './store'
+import MessagesList from './components/MessagesList'
+import NameEntry from './components/NameEntry'
+/**
+ * COMPONENT
+ */
+class Routes extends Component {
+  // componentDidMount() {
+  //   this.props.loadInitialData()
+  // }
+
+  render() {
+    // const {isLoggedIn} = this.props
+
+    return (
+      <div>
+        <NameEntry />
+        <MessagesList />
+      </div>
+    )
+  }
+}
+
+/**
+ * CONTAINER
+ */
+const mapState = state => {
+  return {
+    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
+    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
+    isLoggedIn: !!state.user.id
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    loadInitialData() {
+      dispatch(me())
+    }
+  }
+}
+
+// The `withRouter` wrapper makes sure that updates are not blocked
+// when the url changes
+export default withRouter(connect(mapState, mapDispatch)(Routes))
+
+/**
+ * PROP TYPES
+ */
+Routes.propTypes = {
+  loadInitialData: PropTypes.func.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired
+}

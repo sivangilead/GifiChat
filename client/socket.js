c355aafae996f76a3475b46cac86c66a8b76619a
Sivan Gilead <sivan.gilad@gmail.com>
2018-08-07 21:20:31 -0400

    Fixing duplicate message bug

--- client/socket.js
+++ client/socket.js
@@ -6,21 +6,20 @@ const socket = io(window.location.origin);
 
 socket.on('connect', () => {
   console.log('I am now connected to the server!');
+});
 
-  socket.on('new-message', message => {
-    store.dispatch(getMessage(message));
-  });
+socket.on('new-message', message => {
+  store.dispatch(getMessage(message));
+});
 
-  socket.on('add-guest', list => {
-    console.log('guests', list)
-    store.dispatch(addGuest(list));
-  });
+socket.on('add-guest', list => {
+  console.log('guests', list);
+  store.dispatch(addGuest(list));
+});
 
-  socket.on('remove-guest', newlist => {
-    console.log('remove-guest', newlist)
-    store.dispatch(addGuest(newlist));
-  });
-
+socket.on('remove-guest', newlist => {
+  console.log('remove-guest', newlist);
+  store.dispatch(addGuest(newlist));
 });
 
 export default socket;
--- main.js
+++ main.js
@@ -18,15 +18,15 @@ function createWindow() {
   //   slashes: true
   // });
   // mainWindow.loadURL(startUrl);
-  // mainWindow.loadURL('https://gifichat.herokuapp.com/')
-  mainWindow.loadURL('http://localhost:8080/')
+  mainWindow.loadURL('https://gifichat.herokuapp.com/')
+  // mainWindow.loadURL('http://localhost:8080/')
 
 
   // and load the index.html of the app.
   // mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
